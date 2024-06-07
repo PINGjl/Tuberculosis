b20e95b452eafa4eb03c9c6a8acc4c6b63a78e6b
PINGjl <104889654+PINGjl@users.noreply.github.com>
2024-06-07 16:17:52 +0800

    01_mapping.sh

--- 02_scRNA-seq/01_mapping.sh
+++ 02_scRNA-seq/01_mapping.sh
@@ -1,7 +1,7 @@
 count=/01_result/07_lung/01_ref
 
 cd $count
-for sample in Ctrl-1 Ctrl-2 Ctrl-3 Ctrl-4 Ctrl-5 Ctrl-6 Ctrl-7 AL-TB-1 AL-TB-2 AL-TB-3 L-TB-1 L-TB-2 L-TB-3;
+for sample in Ctrl-1 Ctrl-3 Ctrl-2 Ctrl-4 Ctrl-5 Ctrl-6 Ctrl-7 AL_TB-1 L_TB-1 AL_TB-2 L_TB-2 AL_TB-3 L_TB-3 Ctrl-8 Ctrl-9 Ctrl-10 Ctrl-11 Ctrl-12 Ctrl-13 AL_TB-4 AL_TB-5 AL_TB-6 AL_TB-7 AL_TB-8 AL_TB-9 AL_TB-10 AL_TB-11 AL_TB-12 L_TB-4 L_TB-5 L_TB-6 L_TB-7 L_TB-8 L_TB-9 L_TB-10 L_TB-11 L_TB-12;
 do
 /02_Software/cellranger/cellranger-4.0.0/bin/cellranger count \
 --id=$sample \
